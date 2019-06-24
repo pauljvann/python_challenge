35cb3101c0920376a39dc3ee0f472bbb27571b25
Paul Vann <paul@vanntechcyber.com>
2019-06-24 09:38:41 -0400

    Create config.py
    Choose a directory to create on your system by putting a directory name in the directory variable found here.

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+
+#directory where you would like everything saved and pulled from (for example /Users/pvann/Desktop/python)
+
+directory = '/Users/pvann/Desktop/pythonChal'
+
+if not os.path.exists(directory):
+    os.makedirs(directory)
