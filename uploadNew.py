62e74c56c6b668ef508a7b42573c6444848d7f1c
Paul Vann <paul@vanntechcyber.com>
2019-06-24 10:40:42 -0400

    Update uploadNew.py

--- uploadNew.py
+++ uploadNew.py
@@ -6,8 +6,8 @@ import json
 
 ipArray = []
 
-fileName = input('Input path to text file: ')
-ipText = getIPs(fileName)
+fileName = input('Input path to text file: ') 
+ipText = getIPs(fileName) # takes text file of ip's and random text as input
 
 with open(ipText, 'r') as file:
     lineArray = file.readlines()
@@ -15,10 +15,10 @@ with open(ipText, 'r') as file:
 for line in lineArray:
     jsonLine = json.loads(line)
     for ip in jsonLine:
-        ipArray.append(ip)
+        ipArray.append(ip) #appends IP's in text file to ip Array
 
-geoFile = jsonGEO(ipArray)
+geoFile = jsonGEO(ipArray) #pulls geo data
 
-rdapFile = getRDAPtxt(ipArray)
+rdapFile = getRDAPtxt(ipArray) #pulls RDAP data
 
-import existingData
+import existingData #allows users to run queries on data
