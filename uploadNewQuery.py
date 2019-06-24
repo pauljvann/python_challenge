4ce29c9de2bf5e4e63465e3f526e128a92d9fd6a
Paul Vann <paul@vanntechcyber.com>
2019-06-24 10:39:37 -0400

    Update uploadNewQuery.py

--- uploadNewQuery.py
+++ uploadNewQuery.py
@@ -8,7 +8,7 @@ from multiprocessing import Process
 
 ipArray = []
 
-fileName = input('Input path to text file: ')
+fileName = input('Input path to text file: ') #takes text file of IP's and random text as input
 ipText = getIPs(fileName)
 
 with open(ipText, 'r') as file:
@@ -17,27 +17,27 @@ with open(ipText, 'r') as file:
 for line in lineArray:
     jsonLine = json.loads(line)
     for ip in jsonLine:
-        ipArray.append(ip)
+        ipArray.append(ip) #identifies IP addresses in text file and appends them to array
 
-geoFile = jsonGEO(ipArray)
+geoFile = jsonGEO(ipArray) #pulls GEO data
 print(geoFile)
 
 def pullRDAP():
-    rdapFile = getRDAPtxt(ipArray)
+    rdapFile = getRDAPtxt(ipArray)  #pulls RDAP Data
     return(rdapFile)
 
 i = 0
 
-thread = threading.Thread(target=pullRDAP, args=())
+thread = threading.Thread(target=pullRDAP, args=()) #threads RDAP data to pull data while also allowing for queries
 thread.daemon = True
 thread.start()
 
 print(' ')
 
-from query import queryValidator, queryDataValidator
+from query import queryValidator, queryDataValidator 
 
 
-def userQueryFunc(userType, userQuery):
+def userQueryFunc(userType, userQuery): #query function to allow users to run queries
 
     if userType == 'd':
         response = queryDataValidator(userQuery)
@@ -54,6 +54,6 @@ def userQueryFunc(userType, userQuery):
 
 i = 0
 while i < 1:
-    userType = input("Would you like to pull data or IPs with your query? (d or i)")
-    userQuery = input('Insert Query Here: ')
+    userType = input("Would you like to pull data or IPs with your query? (d or i)") #takes user query type input
+    userQuery = input('Insert Query Here: ') #takes user query content input
     userQueryFunc(userType, userQuery)
